216fb24fd36e071c2cf2668a3bcb32f3b92ba08a
Angelo Julioth <angelojulioth13@hotmail.com>
2016-08-24 02:34:39 -0500

    ADDED break_time.py

---
+++ 02-UseFunctions/break_time.py
@@ -0,0 +1,7 @@
+# Importar la clase webbrowser
+import webbrowser
+
+
+# La función open de la clase webbrowser necesita un argumento string que equivale
+# a un URL que se abre en el navegador por defecto.
+webbrowser.open("https://www.youtube.com/watch?v=z5X5zh00rdg")
